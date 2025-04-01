2b5830dc1f838cc4d98ca285c5462c54cd391e12
mati <matias@keeper.app>
2025-04-01 14:44:58 -0300

    first commit

---
+++ src/main/java/edu/austral/dissis/tictactoe/Main.java
@@ -0,0 +1,8 @@
+package edu.austral.dissis.tictactoe;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/test/java/edu/austral/dissis/tictactoe/ExampleTest.java
@@ -0,0 +1,13 @@
+package edu.austral.dissis.tictactoe;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+  @Test
+  public void test() {
+    assertTrue(true);
+  }
+}
